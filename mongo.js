341792cc085b3bd7ed974283c06f73c1071af70e
Lakshita Saxena <97253226+LakshitaS@users.noreply.github.com>
2023-06-14 18:58:21 +0530

    Add files via upload

---
+++ mongo.js
@@ -0,0 +1,24 @@
+const mongoose=require("mongoose")
+mongoose.connect("mongodb+srv://user123:password123Tech@cluster0.yf5hys8.mongodb.net/react-login-tut?retryWrites=true&w=majority")
+.then(()=>{
+    console.log("mongodb connected");
+})
+.catch(()=>{
+    console.log('failed');
+})
+
+
+const newSchema=new mongoose.Schema({
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+})
+
+const collection = mongoose.model("collection",newSchema)
+
+module.exports=collection
